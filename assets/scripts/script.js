f3066bf6ebf3b28ecf55166b649074c07ea9099e
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-01-31 22:50:34 -0700

    Retrieving data from the API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,30 +11,42 @@ var formSubmitHandler = function(event) {
 }
 
 
-var getCityWeather = function(lat, lon) {
+var getCityWeather = function() {
+    //create the variable for the value of the input from the user which is the name of teh city
+    // search for latitud and the lon of the city and replace in the variables
     var lat = 32.71571;
     var lon = -117.16472;
+    var units = "imperial";
     var apiKey = '4bba11dfee0efb4005da5f8d661387b5';
-    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
     
 
     fetch(apiUrl)
-      .then(function (response){
-        if(response.ok) {
-            response.json().then(function (data){
-                displayWeather(data.list);
-            });
-        } else {
-            alert('Error: ' + response.statusText);
-        }
-      });
-};
-
-var displayWeather = function() {
-    /* if() {
-        
-    } */
+        .then(function (response){
+        return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            for(var i = 0; i < data.list.length; i++) {
+                console.log("City :" + data.city.name);
+                console.log("Time: " + data.list[i].dt_txt);
+                console.log("Temperature: " + data.list[i].main.temp);
+                console.log("Wind: " + data.list[i].wind.speed + "MPH");
+                console.log("Humidity: " + data.list[i].main.humidity);
+            }
+        });
 }
+/* var displayActualWeather = function(city) {
+    
+    for (var i = 0; i < city.length; i++) {
+        var listItemEl = document.createElement('li');
+        listItemEl.classList = 'list-group-item flex row';
+        listItemEl.textContent = city[i].
+
+    }
+} */
 
 
+getCityWeather();
 userFormEl.addEventListener('submit', formSubmitHandler);
